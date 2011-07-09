961d9f26e27695c1fe6050671628dc258cc8f3c7
shashank <shashank_shetty@yahoo.com>
2011-07-09 16:09:24 -0500

    added validation to product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,56 @@
 require 'spec_helper'
 
 describe Product do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @attr = {:title => "My Book Title", :description => "My Book Description", :image_url => "book.jpg", :price => 2.0}
+  end
+
+  it "should not a create a new instance if the attributes are empty" do
+    product = Product.new
+    product.should_not be_valid
+    product.errors[:title].should_not be_empty
+    product.errors[:description].should_not be_empty
+    product.errors[:price].should_not be_empty
+    product.errors[:image_url].should_not be_empty
+  end
+
+  it "should not create a product with negative price" do
+    product = Product.new(@attr.merge(:price => -1))
+    product.should_not be_valid
+    product.errors[:price].join('; ').should == "must be greater than or equal to 0.01"
+  end
+
+  it "should not create a product with price equal to 0" do
+    product = Product.new(@attr.merge(:price => 0))
+    product.should_not be_valid
+    product.errors[:price].join('; ').should == "must be greater than or equal to 0.01"
+    end
+
+  it "should create a product with price greater than 0" do
+    product = Product.new(@attr.merge(:price => 2))
+    product.should be_valid
+  end
+
+  it "should not create a product without valid image url" do
+    image_urls = %w[test.doc test.gif.more]
+    image_urls.each do |image_url|
+      product = Product.new(@attr.merge(:image_url => image_url))
+      product.should_not be_valid
+    end
+  end
+
+  it "should not create a product with valid image url" do
+    image_urls = %w[test.gif test.jpg]
+    image_urls.each do |image_url|
+      product = Product.new(@attr.merge(:image_url => image_url))
+      product.should be_valid
+    end
+  end
+
+  it "should not create a product without a unique title" do
+    product = Product.create!(@attr)
+    product = Product.new(@attr.merge(:title => "My Book Title"))
+    product.should_not be_valid
+    product.errors[:title].join('; ').should == I18n.translate('activerecord.errors.messages.taken')
+  end
 end
--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -20,13 +20,9 @@ describe "products/index.html.erb" do
 
   it "renders a list of products" do
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Image Url".to_s, :count => 2
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "9.99".to_s, :count => 2
+    assert_select "tr>td>img", :src => "Image Url".to_s, :count => 2
+    assert_select "dl>dt", :text => "Title".to_s, :count => 2
+    assert_select "dl>dd", :text => "MyText".to_s, :count => 2
+    #assert_select "tr>td", :text => "9.99".to_s, :count => 2
   end
 end
